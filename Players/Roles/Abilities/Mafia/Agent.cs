2b341d8e6b28a4ca7c0b7d1f360d7145031e247c
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-05-08 11:55:55 +0200

    Added mafia base ability and agent ability

---
+++ Players/Roles/Abilities/Mafia/Agent.cs
@@ -0,0 +1,49 @@
+﻿using Mafia.NET.Matches.Chats;
+using System;
+using System.Collections.Generic;
+
+namespace Mafia.NET.Players.Roles.Abilities.Mafia
+{
+    [RegisterAbility("Agent", typeof(AgentSetup))]
+    public class Agent : MafiaAbility<AgentSetup>
+    {
+        protected override void _onNightStart()
+        {
+            TargetManager.Add(TargetFilter.Living(Match));
+        }
+
+        protected override void _onNightEnd()
+        {
+            if (TargetManager.Try(0, out var target))
+            {
+                var targetVisited = target.Role.Ability.TargetManager[0];
+                var targetVisitedMessage = targetVisited == null ?
+                    "Your target did not do anything tonight." :
+                    $"Your target visited {targetVisited.Name} tonight.";
+
+                var foreignVisits = new List<string>();
+                foreach (var player in Match.LivingPlayers.Values)
+                {
+                    var visited = player.Role.Ability.TargetManager[0];
+                    if (visited != target) continue;
+
+                    foreignVisits.Add($"{player.Name} visited your target tonight.");
+                }
+
+                if (foreignVisits.Count == 0) foreignVisits.Add("Your target was not visited by anyone tonight.");
+
+                var targetNotification = Notification.Chat(targetVisitedMessage);
+                var foreignNotification = Notification.Chat(string.Join(Environment.NewLine, foreignVisits));
+
+                User.OnNotification(targetNotification);
+                User.OnNotification(foreignNotification);
+            }
+        }
+    }
+
+    public class AgentSetup : IAbilitySetup
+    {
+        public bool becomesMafiosoIfAlone = true;
+        public int nightsBetweenShadowings = 1;
+    }
+}
---
+++ Players/Roles/Abilities/Mafia/MafiaAbility.cs
@@ -0,0 +1,30 @@
+﻿namespace Mafia.NET.Players.Roles.Abilities.Mafia
+{
+    public class MafiaAbility<T> : BaseAbility<T> where T : IAbilitySetup
+    {
+        public static readonly string NightChatName = "Mafia";
+
+        public sealed override void OnDayStart()
+        {
+            base.OnDayStart();
+        }
+
+        public sealed override bool OnDayEnd()
+        {
+            base.OnDayEnd();
+            return Active;
+        }
+
+        public sealed override bool OnNightStart()
+        {
+            if(base.OnNightStart()) Match.ChatManager.Open(NightChatName, User);
+            return Active;
+        }
+
+        public sealed override bool OnNightEnd()
+        {
+            base.OnNightEnd();
+            return Active;
+        }
+    }
+}
