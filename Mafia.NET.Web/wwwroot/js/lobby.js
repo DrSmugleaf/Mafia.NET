9b5bc55e1f0fad54e3b0f00447c3899dfe23928e
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-05-18 12:33:13 +0200

    Added role summary, abilities and goal to setup when clicking on one
    Add and remove buttons

--- Mafia.NET.Web/wwwroot/js/lobby.js
+++ Mafia.NET.Web/wwwroot/js/lobby.js
@@ -4,6 +4,8 @@ const divMessages = document.querySelector("#lobby-messages");
 const messageInput = document.querySelector("#message-input");
 const buttonStart = document.querySelector("#lobby-start");
 const setupRoleList = document.querySelector("#setup-role-list")
+const addRoleButton = document.querySelector("#add-role-button")
+const removeRoleButton = document.querySelector("#remove-role-button")
 
 const connection = new signalR.HubConnectionBuilder()
     .withUrl("/LobbyChat")
@@ -59,10 +61,39 @@ $(".catalog-team").click(function() {
 $(".catalog-role").click(function() {
     const name = $(this).data("name");
     const color = $(this).data("color");
+    const summary = $(this).data("summary");
+    const abilities = $(this).data("abilities");
+    const goal = $(this).data("goal");
     
-    $(setupRoleList).append('<button type="button" class="btn btn-sm list-group-item setup-role disable-on-start" style="color: ' + color + '">' + name + '</button>')
+    $("#role-name").text(name).css("color", color);
+    $("#role-summary").text(summary);
+    $("#role-abilities").text(abilities);
+    $("#role-goal").text(goal);
     
     $(".setup-role").click(function() {
         $(this).remove();
     })
+})
+
+$(addRoleButton).click(function() {
+    const roleEntry = $("#role-name");
+    const roleName = roleEntry.text();
+    if (!roleName) return;
+    
+    const color = roleEntry.css("color");
+    const role = $('<button type="button" class="btn btn-sm list-group-item setup-role disable-on-start" data-name="' + roleName + '" style="color: ' + color + '">' + roleName + '</button>');
+    $(setupRoleList).append(role);
+    
+    role.click(function() {
+        $(this).remove();
+    })
+})
+
+$(removeRoleButton).click(function() {
+    const roleEntry = $("#role-name");
+    const roleName = roleEntry.text();
+    if (!roleName) return;
+    
+    const role = $('.setup-role[data-name="' + roleName + '"]').last();
+    role.remove();
 })
