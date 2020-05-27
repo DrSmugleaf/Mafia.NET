42dedda55433320f528ddb92d135b13bc11be5a9
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-05-27 11:47:50 +0200

    Added crier night chat test

---
+++ Mafia.NET.IntegrationTests/Players/Roles/Abilities/Town/CrierTest.cs
@@ -0,0 +1,38 @@
+﻿using Mafia.Net.IntegrationTests.Matches;
+using Mafia.NET.Matches;
+using Mafia.NET.Matches.Phases;
+using Mafia.NET.Players.Roles.Abilities.Town;
+using NUnit.Framework;
+
+namespace Mafia.Net.IntegrationTests.Players.Roles.Abilities.Town
+{
+    [TestFixture]
+    [TestOf(typeof(Crier))]
+    public class CrierTest : BaseMatchTest
+    {
+        [TestCase("Crier,Citizen,Mafioso")]
+        public void NightChat(string namesString)
+        {
+            var roleNames = namesString.Split(",");
+            var match = new Match(roleNames);
+            match.Start();
+
+            match.Skip<NightPhase>();
+
+            var crier = match.AllPlayers[0];
+            var text = "Hello guys, DJ Crier here";
+            var messages = match.Chat.Send(crier, text);
+            
+            Assert.That(messages.Count, Is.EqualTo(1));
+            
+            foreach (var message in messages)
+            {
+                Assert.That(message.Listeners.Count, Is.EqualTo(roleNames.Length));
+                Assert.That(message.Sender.Owner, Is.EqualTo(crier));
+                Assert.That(message.Text, Is.EqualTo(text));
+                Assert.That(message.Sender.Nickname, Is.Not.Null);
+                Assert.That(message.DisplayText(crier).String, Does.Not.Contain(crier.Name));
+            }
+        }
+    }
+}
