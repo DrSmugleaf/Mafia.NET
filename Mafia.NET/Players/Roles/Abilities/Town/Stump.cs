3f85644b8f5add11c9071af905fbc8965746bccc
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-05-27 20:36:05 +0200

    Added stump role ability

---
+++ Mafia.NET/Players/Roles/Abilities/Town/Stump.cs
@@ -0,0 +1,11 @@
+﻿namespace Mafia.NET.Players.Roles.Abilities.Town
+{
+    [RegisterAbility("Stump", typeof(StumpSetup))]
+    public class Stump : TownAbility<StumpSetup>
+    {
+    }
+
+    public class StumpSetup : ITownSetup
+    {
+    }
+}
