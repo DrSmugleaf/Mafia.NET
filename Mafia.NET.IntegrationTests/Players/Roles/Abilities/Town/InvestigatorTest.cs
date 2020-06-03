877b308356363b8b637f4a72b14d0f21591a7b1b
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2020-06-03 16:41:17 +0200

    Added Consigliere test

--- Mafia.NET.IntegrationTests/Players/Roles/Abilities/Town/InvestigatorTest.cs
+++ Mafia.NET.IntegrationTests/Players/Roles/Abilities/Town/InvestigatorTest.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using Mafia.Net.IntegrationTests.Matches;
 using Mafia.NET.Localization;
 using Mafia.NET.Matches;
 using Mafia.NET.Matches.Phases;
 using Mafia.NET.Players.Roles.Abilities.Actions;
+using Mafia.NET.Players.Roles.Abilities.Mafia;
 using Mafia.NET.Players.Roles.Abilities.Town;
 using NUnit.Framework;
 
@@ -11,13 +13,11 @@ namespace Mafia.Net.IntegrationTests.Players.Roles.Abilities.Town
 {
     [TestFixture]
     [TestOf(typeof(Investigator))]
+    [TestOf(typeof(Consigliere))]
     public class InvestigatorTest : BaseMatchTest
     {
-        [TestCase("Investigator,Citizen,Citizen", true, true)]
-        [TestCase("Investigator,Citizen,Citizen", true, false)]
-        [TestCase("Investigator,Citizen,Mafioso", false, true)]
-        [TestCase("Investigator,Citizen,Mafioso", false, false)]
-        public void Detect(string rolesString, bool isInnocent, bool exact)
+        [TestCaseSource(typeof(InvestigateCases))]
+        public void Investigate(string rolesString, bool innocent, bool exact)
         {
             var roleNames = rolesString.Split(",");
             var match = new Match(roleNames);
@@ -44,12 +44,37 @@ namespace Mafia.Net.IntegrationTests.Players.Roles.Abilities.Town
             {
                 var citizenName = citizen.Role.Name;
                 Assert.That(target.Crimes.RoleName(),
-                    isInnocent ? Is.EqualTo(citizenName) : Is.Not.EqualTo(citizenName));
+                    innocent ? Is.EqualTo(citizenName) : Is.Not.EqualTo(citizenName));
             }
             else
             {
-                Assert.That(target.Crimes.Crime(investigator.Ability, InvestigateKey.Detect).ToString(),
-                    isInnocent ? Does.Contain("innocent") : Does.Contain("guilty"));
+                Assert.That(target.Crimes.Crime(investigator.Ability, InvestigateKey.Detect).ToString(), innocent
+                    ? Does.Contain("innocent")
+                    : Does.Contain("guilty"));
+            }
+        }
+    }
+
+    public class InvestigateCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            var investigators = new[] {"Investigator", "Consigliere"};
+            var targets = new Dictionary<string, bool>
+            {
+                ["Citizen"] = true,
+                ["Serial Killer"] = false
+            };
+                
+            foreach (var investigator in investigators)
+            foreach (var target in targets)
+            {
+                var targetRole = target.Key;
+                var roleNames = $"{investigator},Citizen,{targetRole},Mafioso";
+                var innocent = target.Value;
+                
+                foreach (var exact in new[] {true, false})
+                    yield return new object[] {roleNames, innocent, exact};
             }
         }
     }
